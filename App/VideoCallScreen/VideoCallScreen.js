660aae1826249bb873a443df8b190c7604186534
jraleman <jraleman@bendiburg.com>
2018-06-04 06:12:47 -0700

    Style VideoCallScreen

--- App/VideoCallScreen/VideoCallScreen.js
+++ App/VideoCallScreen/VideoCallScreen.js
@@ -1,20 +1,63 @@
 import React from 'react';
 import {
+  Text,
+  TextInput,
   View,
-  Text
+  LayoutAnimation,
+  TouchableHighlight,
+  Image,
+  Platform
 } from 'react-native';
+import {
+  QuickbloxLocalVideoView,
+  QuickbloxRemoteVideoView
+} from 'react-native-video-quickblox';
 import styles from './styles';
 
 export default class VideoCallScreen extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {}
+    this.state = {
+      bottom: 1
+    }
   }
   render () {
     return (
-      <View>
-        <Text>{ "VideoCallScreen" }</Text>
+      <View style={styles.container}>
+        <Text style={styles.iconTitle}>{ setTime(this.props.callingTime) }</Text>
+        <QuickbloxRemoteVideoView style={styles.callDetails}>
+          {Platform.OS === 'android' ? <QuickbloxLocalVideoView
+              style={[styles.userVideo, {bottom: this.state.bottom}]}
+              onRendered={() => this.setState({bottom: 0})}/>
+            : <QuickbloxLocalVideoView style={styles.userVideo}/>
+          }
+        </QuickbloxRemoteVideoView>
+        <View style={styles.callButtonContainer}>
+          <TouchableHighlight
+            onPress={() => { alert('End call')}}
+            underlayColor={'transparent'}>
+            <View style={styles.buttonContainer}>
+              <Text style={styles.iconTitle}>Decline</Text>
+            </View>
+          </TouchableHighlight>
+        </View>
       </View>
     );
+  }
+}
+
+function setTime(totalSeconds) {
+  let sec = pad(totalSeconds % 60);
+  let min = pad(parseInt(totalSeconds / 60));
+  return min + ' : ' + sec
+}
+
+function pad(val) {
+  let valString = val + '';
+  if (valString.length < 2) {
+    return '0' + valString;
+  }
+  else {
+    return valString;
   }
 }
--- App/VideoCallScreen/styles.js
+++ App/VideoCallScreen/styles.js
@@ -2,9 +2,47 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    flexGrow: 1,
+    flex: 1,
+    backgroundColor: 'white',
+    marginTop: 0,
+  },
+  callDetails: {
+    flex: 3,
+    backgroundColor: '#6969'
+  },
+  userVideo: {
+    position: 'absolute',
+    bottom: 1,
+    right: 0,
+    width: 80,
+    height: 120,
+    backgroundColor: 'green',
+    overflow: 'hidden'
+  },
+  callButtonContainer: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  iconContainer: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    marginBottom: 10
+  },
+  buttonContainer: {
+    alignItems: 'center',
+  },
+  icon: {
+    height: 30,
+    width: 30
+  },
+  iconTitle: {
+    alignItems: 'center',
+    fontSize: 15,
+    alignSelf: 'center'
   }
 });
