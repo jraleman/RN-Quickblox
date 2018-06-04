4b1b91d0f11ca62074985a56687e5a1ba22f9b3a
jraleman <jraleman@bendiburg.com>
2018-06-04 03:43:57 -0700

    Add HomeScreen with call button

---
+++ App/HomeScreen/HomeScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Button,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import styles from './styles';
+
+export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  btnHandler() {
+    alert("It works!");
+  }
+  render() {
+    return (
+      <View style={ styles.container }>
+        <View style={ styles.button }>
+          <TouchableOpacity
+            onPress={ this.btnHandler }
+            style={ styles.button }
+          >
+            <Text style={ styles.text }>
+              { "Video Call" }
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+};
---
+++ App/HomeScreen/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  button: {
+    paddingLeft: 42,
+    paddingRight: 42,
+    paddingTop: 12,
+    paddingBottom: 12,
+    backgroundColor: '#666'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18
+  }
+});
